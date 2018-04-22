dbcd45bddd8efcbb9535154fe2c359f02a0530e4
Janyelson Victor Lacerda de Oliveira <janyelsonvictor@gmail.com>
2018-04-21 22:22:00 -0300

    Adicionado o descompressor, lê os indices de tamanho fixo e gera o código correspondente

---
+++ src/controller/LzwDecompressor.java
@@ -0,0 +1,178 @@
+package controller;
+
+import infra.LzwReader;
+import model.LzwNode;
+import model.LzwTree;
+
+public class LzwDecompressor
+{
+    private LzwTree dictionary = null;
+    private LzwNode currentNode = null;
+
+    private LzwReader lzwReader;
+
+    private int[] bufferReader;
+    private static final int NBYTES = 2;
+    private int nBits = 8;
+
+    public LzwDecompressor(String filePath)
+    {
+        lzwReader = new LzwReader(filePath);
+        bufferReader = new int[NBYTES];
+
+    }
+
+    /**
+     * Inicia o algoritimo LZW para descomprimir.
+     * É criado o dicionário inicial e, depois, é feito a decodificação.
+     */
+    public void run()
+    {
+        try {
+            createDictionary(lzwReader.getDictionarySize(), true);
+            String code = readAndSearch();
+            System.out.println("Codigo decodificado: " + code);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Função que cria o dicionário, caso overwrite seja true
+     * A função irá criar um novo dicionário, descartando o
+     * dicionário anterior.
+     *
+     * @param size
+     * @param overwrite
+     */
+    private void createDictionary(int size, boolean overwrite){
+        if(dictionary == null || overwrite){
+            dictionary = new LzwTree(size);
+        }
+    }
+
+    /**
+     * Função que recolhe o símbolo lido na entrada e busca na árvore do dicionário,
+     * Caso o símbolo lido não esteja presente no dicionário, acrescenta ao código
+     * de saída o índice do nó atual e inicia a busca deste símbolo pela raiz da árvore.
+     *
+     * @return
+     */
+    private String readAndSearch() throws Exception
+    {
+
+        String code = "";
+        Integer[] indexBR = new Integer[3];
+        indexBR[0] = 7;
+        indexBR[1] = 0;
+        indexBR[2] = 0;
+
+
+        bufferReader = lzwReader.nextBytes(NBYTES);
+        if(bufferReader[0] == -1) return "";
+
+        int index = getIndex(indexBR);
+
+        while(true)
+        {
+            if(searchAndAddSymbol(index))
+            {
+                index = getIndex(indexBR);
+
+                if (indexBR[2] == 1){
+                    code += currentNode.getCode(); //Adiciona o índice do ultimo símbolo lido
+                    break;
+
+                }
+            } else {
+                code += currentNode.getCode();
+                currentNode = null;
+            }
+        }
+
+
+        return code;
+
+    }
+
+    /**
+     * Função que busca o indice a partir do nó atual (ultimo nó percorrido),
+     * caso o nó atual seja null, inicia a busca pela raiz da árvore dicionário,
+     * caso o nó atual possua um filho com o indice procurado, este passa a ser
+     * o nó atual e é retornado true (indicando que o símbolo está presente no dicionário),
+     * caso o nó atual não possua filhos com o indice procurado, é adicionado um
+     * novo filho a este nó com o simbolo de acordo com o próximo indice e é retornado false.
+     *
+     * @param index
+     * @return
+     */
+    private boolean searchAndAddSymbol(int index){
+
+        if(currentNode == null) {
+            currentNode = dictionary.getNodeByIndex(index);
+            System.out.println(currentNode.getDescription());
+            return true;
+        } else if(currentNode.findIndexSymbol(index) != -1){
+            currentNode = currentNode.getNodeByIndex(index);
+            System.out.println(currentNode.getDescription());
+            return true;
+        } else {
+            dictionary.addSymbol(currentNode, index);
+            if(Math.pow(2, nBits)-1 <= dictionary.getSize()) nBits++;
+            System.out.println(currentNode.getDescription());
+            return false;
+        }
+
+    }
+
+    /**
+     * Extrai o próximo indice do buffer a partir das variáveis indexBR que demarcam em que posição do buffer (indexBR) a leitura
+     * irá começar/retornar.
+     *
+     * indexBR[0] indica a posição no buffer
+     * indexBR[1] indica qual byte está lendo do buffer
+     * indexBR[2] indica se a leitura terminou(1) ou não(0)
+     * @param indexBR
+     * @return
+     */
+    private int getIndex(Integer[] indexBR) throws Exception
+    {
+        int index = 0;
+
+        for(int i = nBits-1; i >= 0; i--)
+        {
+            index += getBit(bufferReader[indexBR[1]], indexBR[0])*Math.pow(2, i);
+
+            indexBR[0]--;
+            if(indexBR[0] == -1)
+            {
+                indexBR[1]++;
+                if(indexBR[1] == NBYTES)
+                {
+                    bufferReader = lzwReader.nextBytes(NBYTES);
+                    indexBR[1] = 0;
+                }
+
+                if(bufferReader[indexBR[1]] == -1)
+                {
+                    indexBR[2] = 1;
+                    break;
+                }
+
+                indexBR[0] = 7;
+            }
+        }
+
+        return index;
+    }
+
+    /**
+     * Extrai o bit k do inteiro n e retorna se o bit é 1 ou 0
+     * @param n, k
+     * @return
+     */
+    private int getBit(int n, int k) {
+        return (n >> k) & 1;
+    }
+}
